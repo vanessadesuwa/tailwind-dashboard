cc081e423e2b351cdff917a83852a4821e98319c
Vanessa Omoragbon <vanessa.omoragbon@outlook.com>
2025-09-28 21:06:25 +0100

    js edit

--- script.js
+++ script.js
@@ -3,13 +3,24 @@ const openOverlay = document.getElementById("hamburger-icon");
 const sideBar = document.querySelector(".overlay-wrapper");
 const dropdownIcon = document.querySelector(".drop-down");
 const miniMenu = document.querySelector(".mini-menu");
+const overLay = document.querySelector(".overlay");
 
 openOverlay.addEventListener("click", () => {
   sideBar.classList.toggle("display");
 });
 
 closeOverlay.addEventListener("click", () => {
-  sideBar.classList.toggle("display");
+  sideBar.classList.remove("display");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 1024) {
+    sideBar.classList.remove("display");
+  }
+});
+
+overLay.addEventListener("click", () => {
+  sideBar.classList.remove("display");
 });
 
 dropdownIcon.addEventListener("click", () => {
